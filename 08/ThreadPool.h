1fd22f1b63f4eaef82cadef41b96aa61692cfd79
Catherine Fish <katerinasheps@gmail.com>
2020-12-02 23:09:38 +0300

    Added HW_8 with codestyle

--- 08/ThreadPool.h
+++ 08/ThreadPool.h
@@ -1,24 +1,30 @@
 #include <thread>
+
 #include <vector>
+
 #include <queue>
+
 #include <functional>
+
 #include <mutex>
+
 #include <condition_variable>
+
 #include <atomic>
+
 #include <future>
 
-class ThreadPool
-{
-    std::vector<std::thread> my_threads;
-    std::queue<std::function<void()>> tasks;
+class ThreadPool {
+    std::vector < std::thread > my_threads;
+    std::queue < std:: function < void() >> tasks;
     std::mutex get_from_queue;
     std::condition_variable cond_for_notification;
-    std::atomic<bool> work_cond = false;
+    std::atomic < bool > work_cond = false;
 
-public:
-    explicit ThreadPool(size_t poolSize);
-    template <class Func, class... Args>
-    auto exec(Func func, Args... args) -> std::future<decltype(func(args...))>;
+    public:
+        explicit ThreadPool(size_t poolSize);
+    template < class Func, class...Args >
+        auto exec(Func func, Args...args) -> std::future < decltype(func(args...)) > ;
     ~ThreadPool();
 };
 
--- 08/test.cpp
+++ 08/test.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
+
 #include <cassert>
+
 #include "ThreadPool.h"
 
 
 struct A {};
 
-void foo(const A&) {}
+void foo(const A & ) {}
 
-auto odd(size_t n)
-{
+auto odd(size_t n) {
     return n * n;
 }
 
-auto even(size_t n)
-{
+auto even(size_t n) {
     return n / 2;
 }
 
-
-void DefaultTest (void)
-{   
+void DefaultTest(void) {
     ThreadPool pool(8);
     auto task1 = pool.exec(foo, A());
     task1.get();
-    
-    auto task2 = pool.exec([]() { return 1; });
+
+    auto task2 = pool.exec([]() {
+        return 1;
+    });
     int res = task2.get();
     assert(res == 1);
     return;
 }
 
-void OneMoreTest()
-{
+void OneMoreTest() {
     ThreadPool pool(16);
-    for (size_t i = 0; i < 16; i++)
-    {
-        if (i % 2)
-        {
+    for (size_t i = 0; i < 16; i++) {
+        if (i % 2) {
             auto cur_task = pool.exec(odd, i);
             assert(cur_task.get() == i * i);
         } else {
